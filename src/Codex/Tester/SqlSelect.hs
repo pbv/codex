4684a68f7bd39f173c35c0e12b50a84f274763d9
Fábio Domingues <fdomingues@dcc.fc.up.pt>
2018-05-28 14:59:08 +0100

    Added SQL Select evaluator.

--- src/Codex/Tester/SqlSelect.hs
+++ src/Codex/Tester/SqlSelect.hs
@@ -5,39 +5,31 @@ module Codex.Tester.SqlSelect (
 
 import           Codex.Tester
 import           Data.Text(Text)
-import qualified Data.Text as T
 
 
 sqlSelectTester :: PageInfo -> Code -> Test Result
-sqlSelectTester (PageInfo path meta) (Code lang src) = do
+sqlSelectTester (PageInfo _ meta) (Code lang src) = do
   guard (lang == "sql")
   guard (tester meta == Just "select")
   ---
   evaluator <- configured "language.sql.select.evaluator"
   limits  <- getLimits "language.sql.select.limits"
-  let answerFilePath = getAnswerFilePath (PageInfo path meta)
-  assert (fileExists answerFilePath)
-      ("doctest file not found: " <> show answerFilePath)
-  chmod readable answerFilePath
+  let db_host = maybe [] (\x -> ["--host=" ++ x]) (lookupFromMeta "db:host" meta)
+  let db_user = maybe [] (\x -> ["--user=" ++ x]) (lookupFromMeta "db:user" meta)
+  let db_name = maybe [] (\x -> ["--db=" ++ x]) (lookupFromMeta "db:name" meta)
+  let sql_answer = maybe "" id (lookupFromMeta "sql:answer" meta)
   withTemp "submit.sql" src $ \submittedFilePath -> do
     chmod readable submittedFilePath
-    classify <$>
-      safeExec limits evaluator [answerFilePath, submittedFilePath] ""
+    classify <$> safeExec limits evaluator
+      (db_host ++ db_user ++ db_name ++ ["--query2-file", sql_answer, submittedFilePath]) ""
+
 
 classify :: (ExitCode, Text, Text) -> Result
 classify (_, stdout, stderr)
-  | T.null stdout && match "OK" stderr = accepted stderr
-  | match "Time Limit" stderr          = timeLimitExceeded stderr
-  | match "Memory Limit" stderr        = memoryLimitExceeded stderr
-  | match "Exception Raised" stdout    = runtimeError stdout
-  | match "SyntaxError" stderr         = compileError stderr
-  | match "Failed" stdout              = wrongAnswer stdout
+  | match "Accepted" stderr            = accepted stdout
+  | match "Wrong Answer" stderr        = wrongAnswer stdout
+  | match "Runtime Error" stderr       = runtimeError stdout
+  | match "Compile Error" stderr       = compileError stdout
+  | match "Time Limit" stderr          = timeLimitExceeded stdout
+  | match "Memory Limit" stderr        = memoryLimitExceeded stdout
   | otherwise                          = miscError (stdout <> stderr)
-
--- | guess the test path from page metadata or filename
-getAnswerFilePath :: PageInfo -> FilePath
-getAnswerFilePath (PageInfo path meta)
-  = maybe
-       (replaceExtension path ".sql")
-       (takeDirectory path </>)
-       (lookupFromMeta "answer-file" meta)
