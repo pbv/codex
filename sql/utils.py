2c68e83d44163331dd600af8c0441d286b303b88
Fábio Domingues <fdomingues@dcc.fc.up.pt>
2018-07-23 17:06:58 +0100

    SQL Eval - handle SELECT error when submitting multiple statements

--- sql/utils.py
+++ sql/utils.py
@@ -80,7 +80,10 @@ def mysql_error_handler(exception, scope):
         return mysql_compile_error_msg(exception, scope)
     elif exception.sqlstate is not None and exception.sqlstate[:2] in ("22", "23", "42"):
         return mysql_runtime_error_msg(exception, scope)
-    elif exception.msg is not None and "max_statement_time" in exception.msg:
-        return mysql_time_limit_exceeded_msg(exception, scope)
+    elif exception.msg is not None:
+        if "max_statement_time" in exception.msg:
+            return mysql_time_limit_exceeded_msg(exception, scope)
+        if "executing multiple statements" in exception.msg:
+            return mysql_runtime_error_msg("Executing multiple statements is not allowed.", scope)
     else:
         return mysql_system_error_msg(exception, scope)
